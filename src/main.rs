0c55b8569a5e5c22447055cc771d993a60ed7e8e
MagTheDev <jtulek@at-rt.com>
2023-04-17 22:41:36 +0200

    Make Edupage struct accessible

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
-    println!("Hello, world!");
+use edupage_rs::edupage::Edupage;
+
+#[tokio::main]
+async fn main() {
+    let edpg = Edupage::new();
+    let data = edpg.login("skolasvr".to_string(), "JakubTulek".to_string(), "2x3xrx1m6t".to_string()).await.unwrap();
 }
